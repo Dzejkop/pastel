04c187db2f5f1541b9e11bdec6f2b5abb40f8064
Dzejkop <jakubtrad@gmail.com>
2017-05-11 19:47:33 +0200

    CPP style data generation.

--- Src/parse.py
+++ Src/parse.py
@@ -23,6 +23,21 @@ def write_symbol(symbol_name, symbol_data, target_file):
     write_symbol_name(symbol_name, len(symbol_data), target_file)
     write_symbol_data(symbol_name, symbol_data, target_file)
 
+CONFIG_ROW_WIDTH = 10
+
+def write_symbol_cpp_style(symbol_name, symbol_data, target_file):
+    target_file.write("unsigned char %(name)s[] = {\n" % {'name' : symbol_name})
+
+    current_column = 0
+    for byte in symbol_data:
+        target_file.write("0x%(b)X," % {'b' : byte})
+        current_column += 1
+        if current_column >= CONFIG_ROW_WIDTH:
+            current_column = 0
+            target_file.write("\n")
+
+    target_file.write("};\n")
+
 def read_whole_file(path):
     with open(path, 'rb') as f:
         return f.read()
@@ -47,4 +62,4 @@ for m in matches:
 
         file_contents = read_whole_file(m[0][1])
 
-        write_symbol(m[0][0], file_contents, asm_temp)
+        write_symbol_cpp_style(m[0][0], file_contents, asm_temp)
