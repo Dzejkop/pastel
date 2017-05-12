efef85705d7ed454c48ba04811e53a8e4931a0f1
Dzejkop <jakubtrad@gmail.com>
2017-05-12 19:21:09 +0200

    Testing the makefile integration. Lowercase directory names

--- example/source.cpp
+++ example/source.cpp
@@ -4,7 +4,7 @@
 // Can be generated using the --generate_header option
 #include "pastel.h"
 
-COMPILE_IN_DATA_FILE(test_data, "test_data.txt");
+PASTEL_COMPILE(test_data, "test_data.txt");
 
 int main()
 {
