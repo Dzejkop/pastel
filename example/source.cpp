215bf851f98acb81ad35d21fe3603f850696aa85
Dzejkop <jakubtrad@gmail.com>
2017-05-12 22:14:37 +0200

    Multiple source files support

--- example/source.cpp
+++ example/source.cpp
@@ -4,10 +4,15 @@
 // Can be generated using the --generate_header option
 #include "pastel.h"
 
+#include "test.h"
+
 PASTEL_COMPILE(test_data, "test_data.txt");
 
 int main()
 {
   std::cout << test_data.data;
+
+  Test::test_display();
+
   return 0;
 }
